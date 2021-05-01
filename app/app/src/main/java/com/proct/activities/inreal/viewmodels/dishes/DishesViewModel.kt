f21a2b7f2785c03eed91f25df2cf32da72f78c82
Anton Gulyaev <kishmyak2001@gmail.com>
2021-05-01 14:56:44 +0300

    Added work with FragmentDishes.kt

--- app/app/src/main/java/com/proct/activities/inreal/viewmodels/dishes/DishesViewModel.kt
+++ app/app/src/main/java/com/proct/activities/inreal/viewmodels/dishes/DishesViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.proct.activities.inreal.data.model.Category
+import com.proct.activities.inreal.data.model.Dish
 import com.proct.activities.inreal.utils.adapters.DishesViewModelAdapter
 import com.proct.activities.inreal.utils.providers.CategoryAndDishesViewModelProvider
 import kotlinx.coroutines.Dispatchers
@@ -15,14 +15,24 @@ class DishesViewModel(
     var adapter: DishesViewModelAdapter
 ) : ViewModel() {
 
-    private val _dishesList = MutableLiveData<MutableList<Category>>()
-    val dishesList: LiveData<MutableList<Category>>
+    private val _dishesList = MutableLiveData<MutableList<Dish>>()
+    val dishesList: LiveData<MutableList<Dish>>
         get() = _dishesList
 
 
     init {
         viewModelScope.launch(Dispatchers.IO) {
             val listOfDishes = adapter.getDishesList()
+            _dishesList.postValue(listOfDishes.toMutableList())
         }
     }
+
+    fun getDishesList() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val listOfDishes = adapter.getDishesList()
+            _dishesList.postValue(listOfDishes.toMutableList())
+        }
+    }
+
+
 }
--- app/app/src/main/java/com/proct/activities/inreal/views/main/dishes/FragmentDishes.kt
+++ app/app/src/main/java/com/proct/activities/inreal/views/main/dishes/FragmentDishes.kt
@@ -1,22 +1,90 @@
 package com.proct.activities.inreal.views.main.dishes
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.proct.activities.inreal.R
+import com.proct.activities.inreal.adapters.DishCardAdapter
+import com.proct.activities.inreal.data.model.Dish
+import com.proct.activities.inreal.di.ViewModelFactory
+import com.proct.activities.inreal.utils.diffutils.dishes.DiffUtilsDishes
+import com.proct.activities.inreal.viewmodels.dishes.DishesViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class FragmentDishes : Fragment() {
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private lateinit var listener: DishesListener
+    private lateinit var viewModel: DishesViewModel
+    private var adapter: DishCardAdapter? = null
+    private var listOfDishes: List<Dish> = emptyList()
+    private lateinit var mainNavController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main__dishes__dishes, container, false)
+        mainNavController = findNavController()
+
+        val recyclerView =
+            inflater.inflate(
+                R.layout.fragment_main__dishes__dishes,
+                container,
+                false
+            ) as RecyclerView
+
+        viewModel = viewModels<DishesViewModel> { viewModelFactory }.value
+
+        listener = object : DishesListener {
+            override fun onClickCategory(name: String) {
+                mainNavController.navigate(R.id.fragmentDishes)
+                viewModel.getDishesList()
+            }
+
+        }
+
+        adapter = DishCardAdapter(listOfDishes)
+        recyclerView.adapter = adapter
+        val layoutManager = LinearLayoutManager(activity)
+        recyclerView.layoutManager = layoutManager
+        return recyclerView
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        initObservers()
+    }
+
+    private fun initObservers() {
+        viewModel.dishesList.observe(viewLifecycleOwner) {
+            update(it)
+        }
+    }
+
+    private fun update(list: List<Dish>) {
+        if (adapter == null) {
+            adapter = DishCardAdapter(listOfDishes)
+        }
+        val diff = DiffUtil.calculateDiff(DiffUtilsDishes(adapter!!.dishes, list))
+        adapter!!.dishes = list
+        diff.dispatchUpdatesTo(adapter!!)
+    }
+
+    interface DishesListener {
+        fun onClickCategory(name: String)
+    }
+
 
 }
